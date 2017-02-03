5a80c3f39a7c2be1368c0ab6bf6fc701238d8eca
braulio <cmbraulio86@gmail.com>
2017-02-03 01:00:25 -0600

    Creacion de categorias

--- db_examen.sql
+++ db_examen.sql
@@ -1,1 +1,19 @@
-create database examen;
+#Crear la base de datos y ponerla en uso 
+create database examen; 
+use examen;
+
+#tablas 
+
+create table categoria(
+id_categoria int auto_increment,
+nombre varchar (20) not null,
+primary key (id_categoria) 
+);
+
+create table examen(
+id_examen int auto_increment, 
+total_preguntas int, 
+cat_examen int,
+primary key (id_examen),
+foreign key (cat_examen) references categoria(id_categoria) 
+);
