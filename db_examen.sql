65de9c226bcc547af3f5b2e789af9b8fffca72ef
braulio <cmbraulio86@gmail.com>
2017-02-03 18:29:35 -0600

    Actualizacion de consultas a la base de datos

--- db_examen.sql
+++ db_examen.sql
@@ -4,8 +4,9 @@ use examen;
 
 #tablas 
 create table usuario(
-id_usuario int auto_increment,
+id_usuario integer auto_increment,
 nombre_user varchar(50),
+primary key (id_usuario)
 );
 
 create table categoria(
@@ -21,24 +22,55 @@ cat_examen int not null ,
 primary key (id_examen),
 foreign key (cat_examen) references categoria(id_categoria) 
 );
+
+create table respuestas(
+
+id_respuesta int auto_increment,
+respuesta text,
+primary key (id_respuesta)
+
+);
+
 create table preguntas(
 id_pregunta int auto_increment not null, 
 enunciado varchar(255) not null, 
-repuesta_correcta int not null,
-primary key (id_pregunta), 
+res_correcta int not null,
+primary key (id_pregunta),
+foreign key (res_correcta) references respuestas (id_respuesta) 
 );
 
 
 
+
+
 #------------------INSERCIONES------------------- 
-insert into categoria (nombre) values('java');
-insert into categoria (nombre) values('asp.net');
-insert into categoria (nombre) values('php');
-select * from categoria;
-insert into examen(total_preguntas,cat_examen) values (10,2);
-insert into examen(total_preguntas,cat_examen) values (10,3);
+insert into categoria (nombre_cat) values('java');
+insert into categoria (nombre_cat) values('asp.net');
+insert into categoria (nombre_cat) values('php');
+insert into examen(numero_preguntas,cat_examen) values (10,2);
+insert into examen(numero_preguntas,cat_examen) values (10,3);
+insert into examen(numero_preguntas,cat_examen) values (20,3);
+insert into examen(numero_preguntas,cat_examen) values (40,3);
+insert into examen(numero_preguntas,cat_examen) values (50,3);
+insert into examen(numero_preguntas,cat_examen) values (60,3);
+insert into examen(numero_preguntas,cat_examen) values (90,3);
+
+
+
 
 #------------------CONSULTAS---------------------- 
 
+
+
+#-----------------CONSULTAS POR TABLA------------------
+
 select * from examen;
-select categoria.nombre, examen.id_examen, examen.total_preguntas from categoria, examen where examen.cat_examen=categoria.id_categoria;
+
+
+#cantidad de examen por categoria
+select count(*) as total_examen, categoria.nombre_cat from examen,categoria where examen.cat_examen=categoria.id_categoria;
+
+#Examen que pertenecena a cada categoria
+select categoria.nombre_cat, id_examen ,numero_preguntas from categoria,examen where examen.cat_examen=categoria.id_categoria;
+
+
